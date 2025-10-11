dfb686667350dbf46892867e83284b18012d3918
deelaurence <laurie143pelumi@gmail.com>
2025-10-11 20:56:01 +0100

    ww

--- inventory-frontend/src/lib/axios.ts
+++ inventory-frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = window.location.protocol.startsWith('https') 
-  ? 'https://localhost:4000' 
+  ? 'https://inventory-9cwi.onrender.com' 
   : 'http://localhost:4000';
 
 export const api = axios.create({
